f9caf6a715e4da37f3c4b5fda5be8c9007f84062
johnabelardom <abelardomanangan@gmail.com>
2018-06-11 17:02:33 +0800

    Initial commit

---
+++ app/Http/Controllers/PermissionsController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class PermissionsController extends Controller
+{
+    public function __construct() {
+        // Resrict this controller to Authenticated users only
+        ;
+        
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $this->middleware('permission:edit-pages');
+        
+        $permissions = \Permission::all();
+        return view('permissions.index', compact('permissions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('permissions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request, [
+            'name' => 'required|unique:permissions'
+        ]);
+
+        \Permission::create(['name' => $request['name']]);
+
+        $role = Role::find(1); // Sync New Permissions to Super Admin
+        $role->syncPermissions(\Permission::all());
+
+        session()->flash('message', 'Permission was added successfully');
+        return redirect(route('permissions.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Permissions  $permissions
+     * @return \Illuminate\Http\Response
+     */
+    public function show($permissions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Permissions  $permissions
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($permissions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Permissions  $permissions
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $permissions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Permissions  $permissions
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($permissions)
+    {
+        //
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+
+    <div class="container bg-success text">
+        dhwiuahsiduwhiu
+    </div>
+
+@stop
+
+@section('css')
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+
+
+@section('js')
